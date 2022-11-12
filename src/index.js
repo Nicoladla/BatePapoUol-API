327bea35b35d7ccafc2aa95b18f9a63c289bee48
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-12 19:23:05 -0300

    Variáveis Schema, feita

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import express, { application } from "express";
+import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
+import joi from "joi";
 
 const app = express();
 app.use(express.json());
@@ -10,7 +11,23 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 await mongoClient.connect();
-const db = mongoClient.db();
+const db = mongoClient.db("bate-bapo-uol");
+
+const userSchema = joi.object({
+  name: joi.string().required(),
+  lastStatus: joi.number().required(),
+});
+const messageSchema = joi.object({
+  from: joi.string().required(),
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required(),
+  time: joi.string().required(),
+});
+
+app.post("", async (req, res) => {
+
+})
 
 app.listen(process.env.PORT_EXPRESS, () =>
   console.log(`App running on port ${process.env.PORT_EXPRESS}`)
