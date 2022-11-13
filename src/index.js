6a5df03ce02fd1203a831c1085769b81bdd4a9cb
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-12 21:16:22 -0300

    Validação do nome do participante, verificação da existência do nome do participante e incerção no banco de dados

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 await mongoClient.connect();
-const db = mongoClient.db("bate-bapo-uol");
+const db = mongoClient.db("batepapo-uol");
 
 const userSchema = joi.object({
-  name: joi.string().required(),
-  lastStatus: joi.number().required(),
+  name: joi.string().required().min(3),
 });
 const messageSchema = joi.object({
   from: joi.string().required(),
@@ -25,9 +24,33 @@ const messageSchema = joi.object({
   time: joi.string().required(),
 });
 
-app.post("", async (req, res) => {
+app.post("/participants", async (req, res) => {
+  const user = req.body;
 
-})
+  const { error } = userSchema.validate(user, { abortEarly: true });
+
+  if (error) {
+    return res.sendStatus(422);
+  }
+
+  try {
+    const userExist = await db.collection("users").findOne({ name: user.name });
+
+    if (userExist) {
+      return res.sendStatus(409);
+    }
+
+    await db
+      .collection("users")
+      .insertOne({ name: user.name, lastStatus: Date.now() });
+  
+      //Ainda falta enviar a msg de status, instalar a biblioteca dayjs.
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(process.env.PORT_EXPRESS, () =>
   console.log(`App running on port ${process.env.PORT_EXPRESS}`)
